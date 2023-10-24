a870205d4115ea16d456148d99c18c7ead8a992c
Alessio Hu <kirbyalessio@yahoo.it>
2023-10-24 17:39:39 +0200

    Fix error

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const PORT = 4000;
 
 const app = express();
-app.use(cors()); 
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.status(200).json("Welcome, your app is working well");
@@ -13,58 +13,67 @@ app.get("/", (req, res) => {
 
 app.get("/decksFound/:id", async (req, res) => {
 	const card = req.params.id;
-	const { data } = await axios.get(
-		`https://ygoprodeck.com/api/card/decksFound.php?cardnumber=${card}`
-	);
-	let main = [];
-	let extra = [];
-	let side = [];
-	for (const deck of data) {
-		const resp = await axios.get(
-			`https://ygoprodeck.com/deck/${deck.pretty_url}`
+	try {
+		const { data } = await axios.get(
+			`https://ygoprodeck.com/api/card/decksFound.php?cardnumber=${card}`
 		);
-		resp.data.split("\n").forEach((_) => {
-			if (_.includes("var maindeckjs = '")) {
-				main = [
-					...main,
-					...JSON.parse(
-						_.split("var maindeckjs = '")[1].replace("';", "")
-					),
-				];
-			}
-			if (_.includes("var extradeckjs = '")) {
-				extra = [
-					...extra,
-					...JSON.parse(
-						_.split("var extradeckjs = '")[1].replace("';", "")
-					),
-				];
-			}
-			if (_.includes("var sidedeckjs = '")) {
-				side = [
-					...side,
-					...JSON.parse(
-						_.split("var sidedeckjs = '")[1].replace("';", "")
-					),
-				];
-			}
+		let main = [];
+		let extra = [];
+		let side = [];
+		for (const deck of data) {
+			const resp = await axios.get(
+				`https://ygoprodeck.com/deck/${deck.pretty_url}`
+			);
+			resp.data.split("\n").forEach((_) => {
+				if (_.includes("var maindeckjs = '")) {
+					main = [
+						...main,
+						...JSON.parse(
+							_.split("var maindeckjs = '")[1].replace("';", "")
+						),
+					];
+				}
+				if (_.includes("var extradeckjs = '")) {
+					extra = [
+						...extra,
+						...JSON.parse(
+							_.split("var extradeckjs = '")[1].replace("';", "")
+						),
+					];
+				}
+				if (_.includes("var sidedeckjs = '")) {
+					side = [
+						...side,
+						...JSON.parse(
+							_.split("var sidedeckjs = '")[1].replace("';", "")
+						),
+					];
+				}
+			});
+		}
+		let ydk = "#created by Ale\n#main\n";
+		main.forEach((_) => (ydk += _ + "\n"));
+
+		ydk += "#extra\n";
+		extra.forEach((_) => (ydk += _ + "\n"));
+
+		ydk += "!side\n";
+		side.forEach((_) => (ydk += _ + "\n"));
+
+		return res.json({
+			main,
+			extra,
+			side,
+			ydk,
+		});
+	} catch (e) {
+		return res.json({
+			main: [],
+			extra: [],
+			side: [],
+			ydk: "",
 		});
 	}
-	let ydk = "#created by Ale\n#main\n";
-	main.forEach((_) => (ydk += _ + "\n"));
-
-	ydk += "#extra\n";
-	extra.forEach((_) => (ydk += _ + "\n"));
-
-	ydk += "!side\n";
-	side.forEach((_) => (ydk += _ + "\n"));
-
-	return res.json({
-		main,
-		extra,
-		side,
-		ydk
-	});
 });
 
 app.listen(PORT, () => {
